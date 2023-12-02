fee776596ea61cf4c2e1a010f4212511145c8372
ThaysonScript <thaysonguedes01@gmail.com>
2023-12-02 15:14:21 -0300

    Revert "Merge remote-tracking branch 'CRUD-C/ALTERANDO-MENU' into ALTERANDO-MENU"
    This reverts commit f1f59b183abcdf564a5a1548ce1a5fc1a3624953, reversing
    changes made to b597ce85c12b0dfc837aaffb549a5d0dd6a9e9d1.

--- src/cabecalho.h
+++ src/cabecalho.h
@@ -24,12 +24,9 @@ void aumentarMemoria(cadAluno **cadastroAluno);
 
 int moverDireitaEinserir ( cadAluno **cadastroAluno, cadAluno registro);
 int moverFimEinserir ( cadAluno **cadastroAluno, cadAluno registro);
-int removerPosicao(int posicao, cadAluno **cadastroAluno);
-int removerRegistro ( int id, cadAluno **cadastroAluno);
+int moverLugarEscolhido (cadAluno **cadastroAluno, cadAluno registro, int posicao);
 
 // ASSINATURA DE FUNÇÕES: INSERÇÃO DE REGISTRO NAS POSIÇÕES ESCOLHIDAS PELO USUÁRIO //
 int inserirInicio(cadAluno **cadastroAluno, cadAluno registro);
 int inserirFim(cadAluno **cadastroAluno, cadAluno registro);
 int inserirPosiInformada (cadAluno **cadastroAluno, cadAluno registro, int posicao);
-int moverLugarEscolhido (cadAluno **cadastroAluno, cadAluno registro, int posicao);
-int apagarRegistro ( cadAluno **cadastroAluno, int posicao);
